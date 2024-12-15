8b828a41ed7c0fc2276ae6712d154a1fedc3e750
ulukbekov05 <samaganuluukvekov@gmail.com>
2024-12-15 16:30:22 +0600

    egfybyuv

---
+++ main.py
@@ -0,0 +1,29 @@
+def numbers(s):
+    name=set(s)
+
+
+    name1=[]
+    for i in name:
+        if s.count(i)>1:
+          name1.append(i)
+    return list(name1)
+
+
+
+
+
+print(numbers([5, 3, 4, 5, 5, 3, 7]))def numbers(s):
+    name=set(s)
+
+
+    name1=[]
+    for i in name:
+        if s.count(i)>1:
+          name1.append(i)
+    return list(name1)
+
+
+
+
+
+print(numbers([5, 3, 4, 5, 5, 3, 7]))d 
